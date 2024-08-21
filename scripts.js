1403a18a809e0406a9c0a8254be4549b43124f60
LeoScheresDev <leo84scheres84@gmail.com>
2024-08-20 22:46:05 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// document = ir no HTML
+// queryselector = busca o que eu quiser que ele busque
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario(){
+    formulario.style.left = "50%"
+
+}
+
+function desaparecerformulario(){
+    formulario.style.left = "-50%"
+
+}
